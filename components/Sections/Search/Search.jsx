b8bc3465bb9da4a8a466d3e9cb602bbe88771590
sparmankhan04 <sparmankhan04@gmail.com>
2024-06-28 19:11:44 +0000

    multi search result added

--- components/Sections/Search/Search.jsx
+++ components/Sections/Search/Search.jsx
@@ -1,58 +1,181 @@
 import Link from "next/link";
 import { CgClose } from "react-icons/cg";
+import { FaHistory } from "react-icons/fa";
 import { FiBarChart2 } from "react-icons/fi";
 import { HiArrowTrendingUp } from "react-icons/hi2";
 import { IoReceiptOutline } from "react-icons/io5";
 
+function Search({ showSearch, setShowSearch, input, setInput }) {
+  return (
+    <section
+      onMouseEnter={() => setShowSearch(true)}
+      className="absolute shadow-xl border-2 w-full -bottom-96 overflow-hidden overflow-y-auto h-96 bg-base-100 rounded-xl p-2"
+    >
+      {/* Last search */}
+      <div className="flex flex-col space-y-2">
+        <h2 className="px-2 py-1 font-bold flex items-center gap-2">
+          {" "}
+          <IoReceiptOutline /> Recent
+        </h2>
+        <li className="flex items-center justify-between w-full">
+          <FaHistory />
+          <Link
+            className="w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1"
+            href={`/`}
+          >
+            ভূতের গল্প
+          </Link>
+          <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300">
+            <CgClose />
+          </span>
+        </li>
+        <li className="flex items-center justify-between w-full">
+          <FaHistory />
+          <Link
+            className="w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1"
+            href={`/`}
+          >
+            ভালোবাসার গল্প
+          </Link>
 
-function Search({showSearch,setShowSearch,input,setInput}) {
-    return (
-        <section onMouseEnter={()=>setShowSearch(true)} className="absolute shadow-xl border-2 w-full -bottom-96 overflow-hidden overflow-y-auto h-96 bg-base-100 rounded-xl p-2">
-                {/* Last search */}
-                <div className="flex flex-col space-y-2">
-                  <h2 className="px-2 py-1 font-bold flex items-center gap-2"> <IoReceiptOutline /> Recent</h2>
-                  <li className="flex items-center justify-between w-full">
+          <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300">
+            <CgClose />
+          </span>
+        </li>
+        <li className="flex items-center justify-between w-full">
+          <FaHistory />
+          <Link
+            className="w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1"
+            href={`/`}
+          >
+            ডিটেকটিভ গল্প
+          </Link>
 
-                  <Link className="border-b w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1" href={`/`}>ভৌতিক গল্প ভৌতিক গল্পভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প </Link>
-                  <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300"><CgClose /></span>
-                  </li>
-                  <li className="flex items-center justify-between w-full">
-
-                  <Link className="border-b w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1" href={`/`}>ভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প ভৌতিক গল্প </Link>
-                  <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300"><CgClose /></span>
-                  </li>
-                  <li className="flex items-center justify-between w-full">
-
-                  <Link className="border-b w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1" href={`/`}>Hello World </Link>
-                  <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300"><CgClose /></span>
-                  </li>
-                  <li className="flex items-center justify-between w-full">
-
-                  <Link className="border-b w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1" href={`/`}>Hello World </Link>
-                  <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300"><CgClose /></span>
-                  </li>
-                </div>
-                {/* Trending */}
-               <div className="mt-2">
-               <h2 className="font-bold flex items-center gap-2"> <HiArrowTrendingUp /> Trending</h2>
-               {/* Trendings */}
-               <div className="flex flex-wrap gap-1">
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভালোবাসার গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভুতের গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />থ্রিলার</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভালোবাসার গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভালোবাসার গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভুতের গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />থ্রিলার</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />ভালোবাসার গল্প</Link>
-               <Link className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200" href={`/`}><FiBarChart2 />গল্প</Link>
-               </div>
-               </div>
-              </section>
-    );
+          <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300">
+            <CgClose />
+          </span>
+        </li>
+        <li className="flex items-center justify-between w-full">
+          <FaHistory />
+          <Link
+            className="w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1"
+            href={`/`}
+          >
+            Hello World{" "}
+          </Link>
+          <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300">
+            <CgClose />
+          </span>
+        </li>
+        <li className="flex items-center justify-between w-full">
+          <FaHistory />
+          <Link
+            className="w-full mr-4 truncate link-hover flex items-center justify-between text-xs md:text-sm px-3 py-1"
+            href={`/`}
+          >
+            Hello World{" "}
+          </Link>
+          <span className="px-2 rounded hover:text-white py-1 hover:bg-rose-500 duration-300">
+            <CgClose />
+          </span>
+        </li>
+      </div>
+      {/* Trending */}
+      <div className="mt-2">
+        <h2 className="font-bold flex items-center gap-2">
+          {" "}
+          <HiArrowTrendingUp /> Trending
+        </h2>
+        {/* Trendings */}
+        <div className="flex flex-wrap gap-1">
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভালোবাসার গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভুতের গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            থ্রিলার
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভালোবাসার গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভালোবাসার গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভুতের গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            থ্রিলার
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            ভালোবাসার গল্প
+          </Link>
+          <Link
+            className="px-2 flex items-center gap-1 py-1 rounded-full border border-zinc-200"
+            href={`/`}
+          >
+            <FiBarChart2 />
+            গল্প
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Search;
--- components/Sections/Search/SearchResults.jsx
+++ components/Sections/Search/SearchResults.jsx
@@ -1,9 +1,195 @@
+import Link from "next/link";
+import { useState } from "react";
+import { BsPlay } from "react-icons/bs";
+
+import { RemoveScroll } from "react-remove-scroll";
+
+
 function SearchResults() {
-    return (
-        <section className="absolute shadow-xl border-2 border-zinc-300 w-full -bottom-96 overflow-hidden overflow-y-auto h-96 bg-white rounded-xl p-2">
-            
-        </section>
-    );
+    const [search,setSearch] = useState('story')
+    
+  return (
+    <RemoveScroll className="absolute shadow-xl border-2 w-full -bottom-96 overflow-hidden overflow-y-auto h-96 bg-base-100 rounded-xl p-2">
+      <div>
+        <div className="flex scroll flex-col gap-2">
+          <div className="flex justify-between sticky z-50 -top-2 bg-base-100 border-b">
+          <button style={{borderBottom:search==='story'?'4px solid darkorchid':''}} onClick={()=>setSearch('story')} className="px-2 py-1 font-bold text-xs sm:text-sm md:text-sm">
+            Stories
+          </button>
+          <button style={{borderBottom:search==='ebook'?'4px solid darkorchid':''}} onClick={()=>setSearch('ebook')} className="px-2 py-1 font-bold text-xs sm:text-sm md:text-sm">
+            eBooks
+          </button>
+          <button style={{borderBottom:search==='audio'?'4px solid darkorchid':''}} onClick={()=>setSearch('audio')} className="px-2 py-1 font-bold text-xs sm:text-sm md:text-sm">
+            Audios
+          </button>
+          <button style={{borderBottom:search==='blog'?'4px solid darkorchid':''}} onClick={()=>setSearch('blog')} className="px-2 py-1 font-bold text-xs sm:text-sm md:text-sm">
+            Blogs
+          </button>
+          </div>
+          <div>
+            {
+                search === 'story' ?  <div>
+                <ul className="flex flex-col gap-2">
+                 {
+                  [...Array(12).keys()].map((story,i)=>{
+                      return  <li key={i} className="flex flex-col gap-2">
+                      <div className="flex gap-2">
+                        <div className="w-20 h-16 rounded-md overflow-hidden">
+                          <img
+                            className="object-cover"
+                            src="https://via.placeholder.com/150/771796"
+                            alt=""
+                          />
+                        </div>
+                        <div className="flex flex-col truncate">
+                          <Link
+                            className="font-bold truncate text-xs sm:text-sm md:text-sm"
+                            href={"/story/12"}
+                          >
+                            Hello world story for user search bar result
+                          </Link>
+                          <div className="flex gap-2">
+                            <p className="text-xs sm:text-sm">
+                              Created: <span>12days ago</span>
+                            </p>
+                            <p className="text-xs sm:text-sm">
+                              {" "}
+                              author: <span>@samrat</span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  })
+                 }
+                </ul>
+              </div>:''
+            }
+            {/* search ebook */}
+            {
+            search === 'ebook' &&  <div>
+            <ul className="grid gap-2 grid-cols-3">
+             {
+              [...Array(12).keys()].map((story,i)=>{
+                  return  <li key={i} className="flex flex-col gap-2">
+                  <div className="flex flex-col gap-2">
+                    <div className="w-full h-44 items-center relative rounded-md overflow-hidden">
+                     
+                      <img
+                        className="object-cover w-full h-full"
+                        src="https://via.placeholder.com/150/771796"
+                        alt=""
+                      />
+                    </div>
+                    <div className="flex flex-col truncate">
+                      <Link
+                        className="font-bold truncate text-xs sm:text-sm"
+                        href={"/story/12"}
+                      >
+                        Hello world story for user search bar result
+                      </Link>
+                      <div className="flex gap-1">
+                        <p className="text-xs truncate sm:text-sm">
+                          <span>12 day ago</span>
+                        </p>
+                        <p className="text-xs truncate sm:text-sm">
+                          {" "}
+                         <span>@samrat</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              })
+             }
+            </ul>
+          </div>
+           }
+            {/* search audio */}
+           {
+            search === 'audio' &&  <div>
+            <ul className="flex flex-col gap-2">
+             {
+              [...Array(12).keys()].map((story,i)=>{
+                  return  <li key={i} className="flex flex-col gap-2">
+                  <div className="flex gap-2">
+                    <div className="w-20 h-16 flex justify-center cursor-pointer items-center relative rounded-md overflow-hidden">
+                        <span className="absolute text-2xl"><BsPlay /></span>
+                      <img
+                        className="object-cover"
+                        src="https://via.placeholder.com/150/771796"
+                        alt=""
+                      />
+                    </div>
+                    <div className="flex gap-1 flex-col truncate">
+                      <Link
+                        className="font-bold truncate text-xs sm:text-sm md:text-sm"
+                        href={"/story/12"}
+                      >
+                        Hello world story for user search bar result
+                      </Link>
+                      <div className="flex gap-2">
+                        <p className="text-xs sm:text-sm">
+                          Created: <span>12days ago</span>
+                        </p>
+                        <p className="text-xs sm:text-sm">
+                          {" "}
+                          author: <span>@samrat</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              })
+             }
+            </ul>
+          </div>
+           }
+
+           {/* search blog */}
+           {
+                search === 'blog' ?  <div>
+                <ul className="flex flex-col gap-2">
+                 {
+                  [...Array(12).keys()].map((story,i)=>{
+                      return  <li key={i} className="flex flex-col gap-2">
+                      <div className="flex gap-2">
+                        <div className="w-20 h-16 rounded-md overflow-hidden">
+                          <img
+                            className="object-cover"
+                            src="https://via.placeholder.com/150/771796"
+                            alt=""
+                          />
+                        </div>
+                        <div className="flex flex-col truncate">
+                          <Link
+                            className="font-bold truncate text-xs sm:text-sm md:text-sm"
+                            href={"/story/12"}
+                          >
+                            Hello world story for user search bar result
+                          </Link>
+                          <div className="flex gap-2">
+                            <p className="text-xs sm:text-sm">
+                              Created: <span>12days ago</span>
+                            </p>
+                            <p className="text-xs sm:text-sm">
+                              {" "}
+                              author: <span>@samrat</span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  })
+                 }
+                </ul>
+              </div>:''
+            }
+          </div>
+        </div>
+      </div>
+    </RemoveScroll>
+  );
 }
 
 export default SearchResults;
