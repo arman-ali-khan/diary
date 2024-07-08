f6cf2f6979ebaae70c8fcfa8b14d7398bf64d3cc
Arman <sparmankhan@gmail.com>
2024-07-08 17:24:27 +0600

    update read page with part api

--- pages/read/[folderId]/[readId].jsx
+++ pages/read/[folderId]/[readId].jsx
@@ -1,19 +1,24 @@
 import ReadLayout from "@/Layout/ReadLayout";
 import Reviews from "@/components/Sections/Comments/Reviews";
+import Loading from "@/components/Spinner/Loading";
 import ReadBottomBar from "@/components/shared/ReadBottomBar";
 import ReadingHeader from "@/components/shared/ReadingHeader";
 import ProgressBar from "@/lib/ProgressBar";
+import { useGetStoryPartByIdQuery } from "@/redux/features/api/storyApi";
+import parse from "html-react-parser";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { BsStarFill, BsStarHalf } from "react-icons/bs";
 import { CgClose } from "react-icons/cg";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function readId() {
+  // router
+  const router = useRouter();
   // redux
-  const storyState = useSelector((state)=>state.storyState)
-  console.log(storyState,'story');
+  const storyState = useSelector((state) => state.storyState);
+
   // toggle
   const [showReview, setShowReview] = useState(false);
   // height
@@ -58,43 +63,57 @@ function readId() {
     };
   }, []);
 
+  //  hide header and bottom bar
+  const [show, setShow] = useState(true);
+  let lastScrollY = 0;
 
-    //  hide header and bottom bar
-    const [show, setShow] = useState(true);
-    let lastScrollY = 0;
-  
-    const controlNavbar = () => {
-      if (window.scrollY > lastScrollY) {
-        // If the scroll is going down, hide the navbar
-        setShow(false);
-      } else {
-        // If the scroll is going up, show the navbar
-        setShow(true);
-      }
-      lastScrollY = window.scrollY;
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      // If the scroll is going down, hide the navbar
+      setShow(false);
+    } else {
+      // If the scroll is going up, show the navbar
+      setShow(true);
+    }
+    lastScrollY = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
-  
-    useEffect(() => {
-      window.addEventListener('scroll', controlNavbar);
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }, [lastScrollY]);
+  }, [lastScrollY]);
 
-    // router
-    const router = useRouter()
+  // get id
+  const path = router.asPath;
+  const storyId = path?.split("/")[2];
+  const id = path?.split("/")[3];
 
-    // get id
-    const storyId = router.asPath
-    const id = storyId?.split('/')[2]
+  // get part data by story id and part id
+  const {
+    data: partData,
+    isLoading: isLoadingPart,
+    isFetching: isPartFetching,
+  } = useGetStoryPartByIdQuery({ storyId, partId: id });
+
+  // Function to test string
+  function isJSON(str) {
+    try {
+      return JSON.parse(str) && !!str;
+    } catch (e) {
+      return false;
+    }
+  }
 
   return (
-    <ReadLayout title={'Part - 3 || অঘোর তান্ত্রিকের অঘোর তান্ত্রিকের অভিশাপ খন্ডনের কাহিনি || Diary'}>
-       <ReadingHeader show={show} storyId={id} />
-      <section
-        className="max-h-[200vh] relative mt-14 mb-44 bg-base-100 h-full min-h-[100%]"
-      >
+    <ReadLayout title={`${partData?.title || "Story read"} || Diary`}>
+      <ReadingHeader show={show} storyId={storyId} />
+      <section className="max-h-[200vh] relative mt-14 mb-44 bg-base-100 h-full min-h-[100%]">
         <ProgressBar />
+       {
+        isPartFetching ? <div className="flex justify-center h-screen"><Loading /></div>
+        :
         <div
           onCopy={(e) => {
             e.preventDefault();
@@ -116,238 +135,119 @@ function readId() {
             />
           )}
           <div className={``} style={{ fontSize: storyState?.fontSize }}>
-            {`আজ নীলের জন্মদিন। নীল আমার মেজোকাকুর ছেলে। ভালো নাম অন্রদীপ। ক্লাস ফোরে পড়ে। আমার
-
-ক্লাস ফাইভ। ছোটোকাকুর মেয়ে মউ। ওর ভালো নাম তনুশ্ী। ওর সবে ক্লাস টু। আমরা তিনজন বসার ঘরে
-
-জমজমাট। নীলের প্রতিটা জন্মদিনে একটা-না-একটা ধুন্ধুমার কাণ্ড হয়। সে খাওয়া-দাওয়াতেই হোক বা
-
-অনুষ্ঠানের আয়োজনেই হোক। আর মেজোকাকুর মাথায় আসেও নানা অবাক করা আইডিয়া
-
- 
-
-একবার আমার জন্মদিনে বাড়িতেই বসিয়েছিলেন ম্যাজিকের আসর। মঞ্চ তৈরি করে, আলোর ঝিকিমিকি
-
-লাগিয়ে সে কী কাণ্ড! সেবার মেজোকাকুর বন্ধু ম্যাজিশিয়ান অভিষেককাকু বিকেলেই বাড়িতে হাজির পুরো
-
-টিম নিয়ে। অভিষেককাকু মেজোকাকুর সঙ্গে কলেজে পড়তেন। এখন ম্যাজিশিয়ান হিসেবে বেশ নামডাক।
-
-তিনি কাগজ ভাঁজ করে কেমন দুটো পায়রা করে দিলেন। আমাকে একটা মুরগির কাঁচা ডিম দু-হাতে মুঠো
-
-করতে বললেন। তারপর সেটা খেয়ে ফেলতে বললেন। আমি তো খাবই না। মঞ্চে প্রায় কেদে ফেলি আর
-
-কি! তখন ম্যাজিককাকু বললেন, 'ঠিক আছে। এবার মুঠো খুলে দেখো তোমার হাতে কী?' আমি ভয়ে ভয়ে
-
-হাত খুলে দেখি, আমার হাতে একটা মস্ত বড়ো সন্দেশ।
-
- 
-
-আমি নীলের কথাটা কিছুতেই বিশ্বাস করতে পারলাম না। ও কথা বিশ্বাস করা যায় না। মেজোকাকু একটা
-ক্লাস ফাইভ। ছোটোকাকুর মেয়ে মউ। ওর ভালো নাম তনুশ্ী। ওর সবে ক্লাস টু। আমরা তিনজন বসার ঘরে
-
-জমজমাট। নীলের প্রতিটা জন্মদিনে একটা-না-একটা ধুন্ধুমার কাণ্ড হয়। সে খাওয়া-দাওয়াতেই হোক বা
-
-অনুষ্ঠানের আয়োজনেই হোক। আর মেজোকাকুর মাথায় আসেও নানা অবাক করা আইডিয়া
-
- 
-
-একবার আমার জন্মদিনে বাড়িতেই বসিয়েছিলেন ম্যাজিকের আসর। মঞ্চ তৈরি করে, আলোর ঝিকিমিকি
-
-লাগিয়ে সে কী কাণ্ড! সেবার মেজোকাকুর বন্ধু ম্যাজিশিয়ান অভিষেককাকু বিকেলেই বাড়িতে হাজির পুরো
-
-টিম নিয়ে। অভিষেককাকু মেজোকাকুর সঙ্গে কলেজে পড়তেন। এখন ম্যাজিশিয়ান হিসেবে বেশ নামডাক।
-
-তিনি কাগজ ভাঁজ করে কেমন দুটো পায়রা করে দিলেন। আমাকে একটা মুরগির কাঁচা ডিম দু-হাতে মুঠো
-
-করতে বললেন। তারপর সেটা খেয়ে ফেলতে বললেন। আমি তো খাবই না। মঞ্চে প্রায় কেদে ফেলি আর
-
-কি! তখন ম্যাজিককাকু বললেন, 'ঠিক আছে। এবার মুঠো খুলে দেখো তোমার হাতে কী?' আমি ভয়ে ভয়ে
-
-হাত খুলে দেখি, আমার হাতে একটা মস্ত বড়ো সন্দেশ।
-
- 
-
-আমি নীলের কথাটা কিছুতেই বিশ্বাস করতে পারলাম না। ও কথা বিশ্বাস করা যায় না। মেজোকাকু একটা
-ক্লাস ফাইভ। ছোটোকাকুর মেয়ে মউ। ওর ভালো নাম তনুশ্ী। ওর সবে ক্লাস টু। আমরা তিনজন বসার ঘরে
-
-জমজমাট। নীলের প্রতিটা জন্মদিনে একটা-না-একটা ধুন্ধুমার কাণ্ড হয়। সে খাওয়া-দাওয়াতেই হোক বা
-
-অনুষ্ঠানের আয়োজনেই হোক। আর মেজোকাকুর মাথায় আসেও নানা অবাক করা আইডিয়া
-
- 
-
-একবার আমার জন্মদিনে বাড়িতেই বসিয়েছিলেন ম্যাজিকের আসর। মঞ্চ তৈরি করে, আলোর ঝিকিমিকি
-
-লাগিয়ে সে কী কাণ্ড! সেবার মেজোকাকুর বন্ধু ম্যাজিশিয়ান অভিষেককাকু বিকেলেই বাড়িতে হাজির পুরো
-
-টিম নিয়ে। অভিষেককাকু মেজোকাকুর সঙ্গে কলেজে পড়তেন। এখন ম্যাজিশিয়ান হিসেবে বেশ নামডাক।
-
-তিনি কাগজ ভাঁজ করে কেমন দুটো পায়রা করে দিলেন। আমাকে একটা মুরগির কাঁচা ডিম দু-হাতে মুঠো
-
-করতে বললেন। তারপর সেটা খেয়ে ফেলতে বললেন। আমি তো খাবই না। মঞ্চে প্রায় কেদে ফেলি আর
-
-কি! তখন ম্যাজিককাকু বললেন, 'ঠিক আছে। এবার মুঠো খুলে দেখো তোমার হাতে কী?' আমি ভয়ে ভয়ে
-
-হাত খুলে দেখি, আমার হাতে একটা মস্ত বড়ো সন্দেশ।
-
- 
-
-আমি নীলের কথাটা কিছুতেই বিশ্বাস করতে পারলাম না। ও কথা বিশ্বাস করা যায় না। মেজোকাকু একটা
-ক্লাস ফাইভ। ছোটোকাকুর মেয়ে মউ। ওর ভালো নাম তনুশ্ী। ওর সবে ক্লাস টু। আমরা তিনজন বসার ঘরে
-
-জমজমাট। নীলের প্রতিটা জন্মদিনে একটা-না-একটা ধুন্ধুমার কাণ্ড হয়। সে খাওয়া-দাওয়াতেই হোক বা
-
-অনুষ্ঠানের আয়োজনেই হোক। আর মেজোকাকুর মাথায় আসেও নানা অবাক করা আইডিয়া
-
- 
-
-একবার আমার জন্মদিনে বাড়িতেই বসিয়েছিলেন ম্যাজিকের আসর। মঞ্চ তৈরি করে, আলোর ঝিকিমিকি
-
-লাগিয়ে সে কী কাণ্ড! সেবার মেজোকাকুর বন্ধু ম্যাজিশিয়ান অভিষেককাকু বিকেলেই বাড়িতে হাজির পুরো
-
-টিম নিয়ে। অভিষেককাকু মেজোকাকুর সঙ্গে কলেজে পড়তেন। এখন ম্যাজিশিয়ান হিসেবে বেশ নামডাক।
-
-তিনি কাগজ ভাঁজ করে কেমন দুটো পায়রা করে দিলেন। আমাকে একটা মুরগির কাঁচা ডিম দু-হাতে মুঠো
-
-করতে বললেন। তারপর সেটা খেয়ে ফেলতে বললেন। আমি তো খাবই না। মঞ্চে প্রায় কেদে ফেলি আর
-
-কি! তখন ম্যাজিককাকু বললেন, 'ঠিক আছে। এবার মুঠো খুলে দেখো তোমার হাতে কী?' আমি ভয়ে ভয়ে
-
-হাত খুলে দেখি, আমার হাতে একটা মস্ত বড়ো সন্দেশ।
-
- 
-
-আমি নীলের কথাটা কিছুতেই বিশ্বাস করতে পারলাম না। ও কথা বিশ্বাস করা যায় না। মেজোকাকু একটা
-ক্লাস ফাইভ। ছোটোকাকুর মেয়ে মউ। ওর ভালো নাম তনুশ্ী। ওর সবে ক্লাস টু। আমরা তিনজন বসার ঘরে
-
-জমজমাট। নীলের প্রতিটা জন্মদিনে একটা-না-একটা ধুন্ধুমার কাণ্ড হয়। সে খাওয়া-দাওয়াতেই হোক বা
-
-অনুষ্ঠানের আয়োজনেই হোক। আর মেজোকাকুর মাথায় আসেও নানা অবাক করা আইডিয়া
-
- 
-
-একবার আমার জন্মদিনে বাড়িতেই বসিয়েছিলেন ম্যাজিকের আসর। মঞ্চ তৈরি করে, আলোর ঝিকিমিকি
-
-লাগিয়ে সে কী কাণ্ড! সেবার মেজোকাকুর বন্ধু ম্যাজিশিয়ান অভিষেককাকু বিকেলেই বাড়িতে হাজির পুরো
-
-টিম নিয়ে। অভিষেককাকু মেজোকাকুর সঙ্গে কলেজে পড়তেন। এখন ম্যাজিশিয়ান হিসেবে বেশ নামডাক।
-
-তিনি কাগজ ভাঁজ করে কেমন দুটো পায়রা করে দিলেন। আমাকে একটা মুরগির কাঁচা ডিম দু-হাতে মুঠো
-
-করতে বললেন। তারপর সেটা খেয়ে ফেলতে বললেন। আমি তো খাবই না। মঞ্চে প্রায় কেদে ফেলি আর
-
-কি! তখন ম্যাজিককাকু বললেন, 'ঠিক আছে। এবার মুঠো খুলে দেখো তোমার হাতে কী?' আমি ভয়ে ভয়ে
-
-হাত খুলে দেখি, আমার হাতে একটা মস্ত বড়ো সন্দেশ।
-
- 
-
-আমি নীলের কথাটা কিছুতেই বিশ্বাস করতে পারলাম না। ও কথা বিশ্বাস করা যায় না। মেজোকাকু একটা
-
-কিছু করেবনই।`}
+            {partData?.description &&
+              parse(
+                isJSON
+                  ? JSON.parse(partData?.description)
+                  : partData?.description
+              )}
           </div>
           {/* Post footer */}
-        <div className="flex justify-center">
-        <ReadBottomBar show={show} />
-        </div>
+          <div className="flex justify-center">
+            <ReadBottomBar show={show} />
+          </div>
           {/* Rating */}
 
           <>
-          <div className="flex z-50 justify-center w-full">
-            <div className="">
-              {/* Page content here */}
-              <button data-tip='Create review'
-                onClick={() => setShowReview(true)}
-                className="bg-gradient fixed bottom-3 right-12 z-50  rounded-full cursor-pointer text-white px-2 py-1 gap-1 flex text-xl items-center"
-              >
-                <BsStarHalf />
-              </button>
-            </div>
-            <div
-              onWheel={handleWheel}
-              style={{ height: height, bottom: showReview ? 0 : -height }}
-              className={`fixed z-50 duration-300 w-full bg-base-100`}
-            >
-              {/* Hide review */}
-              <button
-                hidden={!showReview}
-                onClick={() => setShowReview(false)}
-                className="fixed top-0 left-0 w-screen h-screen -z-50"
-              ></button>
-              <div className="bg-base-300 sticky top-0 flex items-center px-4 justify-between py-2 w-full">
-                <h2 className="text-xl font-bold">রিভিউস সমূহ</h2>
+            <div className="flex z-50 justify-center w-full">
+              <div className="">
+                {/* Page content here */}
                 <button
-                  onClick={() => setShowReview(false)}
-                  className="cursor-pointer "
+                  data-tip="Create review"
+                  onClick={() => setShowReview(true)}
+                  className="bg-gradient fixed bottom-3 right-12 z-50  rounded-full cursor-pointer text-white px-2 py-1 gap-1 flex text-xl items-center"
                 >
-                  <CgClose size={26} />
+                  <BsStarHalf />
                 </button>
               </div>
-              {/* Content */}
-              <div className="container bg-base-100 mx-auto px-3 text-xl overflow-y-auto h-full pb-12 sm:px-5 md:px-6 ">
-                {/* COmment box */}
-                <div className="flex flex-col items-center">
-                  {/* Stars */}
-                  <div className="flex justify-center my-6 items-center cursor-pointer gap-1">
-                    <BsStarFill
-                      onMouseEnter={() => setStars(1)}
-                      onClick={() => setStars(1)}
-                      className={`${
-                        stars > 0 ? "fill-orange-400" : "fill-zinc-300"
-                      }`}
-                      size={34}
-                    />
-                    <BsStarFill
-                      onMouseEnter={() => setStars(2)}
-                      onClick={() => setStars(2)}
-                      className={`${
-                        stars > 1 ? "fill-orange-400" : "fill-zinc-300"
-                      }`}
-                      size={34}
-                    />
-                    <BsStarFill
-                      onMouseEnter={() => setStars(3)}
-                      onClick={() => setStars(3)}
-                      className={`${
-                        stars > 2 ? "fill-orange-400" : "fill-zinc-300"
-                      }`}
-                      size={34}
-                    />
-                    <BsStarFill
-                      onMouseEnter={() => setStars(4)}
-                      onClick={() => setStars(4)}
-                      className={`${
-                        stars > 3 ? "fill-orange-400" : "fill-zinc-300"
-                      }`}
-                      size={34}
-                    />
-                    <BsStarFill
-                      onMouseEnter={() => setStars(5)}
-                      onClick={() => setStars(5)}
-                      className={`${
-                        stars > 4 ? "fill-orange-400" : "fill-zinc-300"
-                      }`}
-                      size={34}
-                    />
+              <div
+                onWheel={handleWheel}
+                style={{ height: height, bottom: showReview ? 0 : -height }}
+                className={`fixed z-50 duration-300 w-full bg-base-100`}
+              >
+                {/* Hide review */}
+                <button
+                  hidden={!showReview}
+                  onClick={() => setShowReview(false)}
+                  className="fixed top-0 left-0 w-screen h-screen -z-50"
+                ></button>
+                <div className="bg-base-300 sticky top-0 flex items-center px-4 justify-between py-2 w-full">
+                  <h2 className="text-xl font-bold">রিভিউস সমূহ</h2>
+                  <button
+                    onClick={() => setShowReview(false)}
+                    className="cursor-pointer "
+                  >
+                    <CgClose size={26} />
+                  </button>
+                </div>
+                {/* Content */}
+                <div className="container bg-base-100 mx-auto px-3 text-xl overflow-y-auto h-full pb-12 sm:px-5 md:px-6 ">
+                  {/* COmment box */}
+                  <div className="flex flex-col items-center">
+                    {/* Stars */}
+                    <div className="flex justify-center my-6 items-center cursor-pointer gap-1">
+                      <BsStarFill
+                        onMouseEnter={() => setStars(1)}
+                        onClick={() => setStars(1)}
+                        className={`${
+                          stars > 0 ? "fill-orange-400" : "fill-zinc-300"
+                        }`}
+                        size={34}
+                      />
+                      <BsStarFill
+                        onMouseEnter={() => setStars(2)}
+                        onClick={() => setStars(2)}
+                        className={`${
+                          stars > 1 ? "fill-orange-400" : "fill-zinc-300"
+                        }`}
+                        size={34}
+                      />
+                      <BsStarFill
+                        onMouseEnter={() => setStars(3)}
+                        onClick={() => setStars(3)}
+                        className={`${
+                          stars > 2 ? "fill-orange-400" : "fill-zinc-300"
+                        }`}
+                        size={34}
+                      />
+                      <BsStarFill
+                        onMouseEnter={() => setStars(4)}
+                        onClick={() => setStars(4)}
+                        className={`${
+                          stars > 3 ? "fill-orange-400" : "fill-zinc-300"
+                        }`}
+                        size={34}
+                      />
+                      <BsStarFill
+                        onMouseEnter={() => setStars(5)}
+                        onClick={() => setStars(5)}
+                        className={`${
+                          stars > 4 ? "fill-orange-400" : "fill-zinc-300"
+                        }`}
+                        size={34}
+                      />
+                    </div>
+                    <div className="w-full md:w-96 lg:w-[596px] gap-1 flex">
+                      <textarea className="px-4 py-2 w-full textarea bg-white h-full rounded border border-blue-600"></textarea>
+                      <button className="px-4 py-2 h-full rounded bg-gradient text-white">
+                        Review
+                      </button>
+                    </div>
+                    {/* All Reviews */}
+                    <div className="flex justify-start w-full py-2 my-6 bg-base-200 rounded-md px-4">
+                      <h2 className="text-lg font-bold">
+                        অন্যান্য রিভিউ সমূহঃ
+                      </h2>
+                    </div>
+                    {/* Reviews */}
+                    <Reviews />
                   </div>
-                  <div className="w-full md:w-96 lg:w-[596px] gap-1 flex">
-                    <textarea className="px-4 py-2 w-full textarea bg-white h-full rounded border border-blue-600"></textarea>
-                    <button className="px-4 py-2 h-full rounded bg-gradient text-white">
-                      Review
-                    </button>
-                  </div>
-                  {/* All Reviews */}
-                  <div className="flex justify-start w-full py-2 my-6 bg-base-200 rounded-md px-4">
-                    <h2 className="text-lg font-bold">অন্যান্য রিভিউ সমূহঃ</h2>
-                  </div>
-                  {/* Reviews */}
-                  <Reviews />
                 </div>
               </div>
             </div>
-          </div>
-            </>
+          </>
         </div>
+       }
       </section>
     </ReadLayout>
   );
